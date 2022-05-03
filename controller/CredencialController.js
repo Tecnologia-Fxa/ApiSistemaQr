030507cc880100e964f28adb46e39adb8ff9d0fc
FSCB21 <freddystibencb@gmail.com>
2022-05-03 16:47:24 -0500

    Se Agregaron respuestas de estado de error en la ruta de login en credenciales (401)

--- controller/CredencialController.js
+++ controller/CredencialController.js
@@ -33,9 +33,9 @@ const CredencialController = {
             if(log)
                 res.json(tokenAcceso(user.nombre_usuario))
             else 
-                res.json("Error usuario y/o contraseña incorrectos")     
+                res.status(401).json("Error usuario y/o contraseña incorrectos")     
         }else
-            res.json("Error usuario y/o contraseña incorrectos")
+            res.status(401).json("Error usuario y/o contraseña incorrectos")
     },
 
     createCredencials: async(_req,res)=>{
