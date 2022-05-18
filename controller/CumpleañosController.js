676ac5af2490f15f8c7b0fdd97d729ced02e11df
Freddy Calderon <freddystibencb@gmail.com>
2022-05-18 15:33:40 -0500

    Se crea la consulta de cumpleañeros por mes

--- controller/CumpleañosController.js
+++ controller/CumpleañosController.js
@@ -7,6 +7,12 @@ const CumpleañosController = {
         const { fecha } = req.query
         const data = await sequelize.query(`select id_usuario, nombres, apellidos, nombre_lugar_registro, telefono_contacto, fecha_nacimiento from usuario left outer join lugar_registro on lugar_registro_fk = id_lugar_registro where day(fecha_nacimiento) = day('${fecha}') and month(fecha_nacimiento) = month('${fecha}')`) 
         res.json(data[0])
+    },
+
+    getByMonth:async(req,res)=>{
+        const { fecha } = req.query
+        const data = await sequelize.query(`select id_usuario, nombres, apellidos, nombre_lugar_registro, telefono_contacto, fecha_nacimiento, day(fecha_nacimiento) dia from usuario left outer join lugar_registro on lugar_registro_fk = id_lugar_registro where month(fecha_nacimiento) = month('${fecha}') order by dia`)
+        res.json(data[0])
     }
 
 }
--- router/routes/CumpleañosRouter.js
+++ router/routes/CumpleañosRouter.js
@@ -4,4 +4,6 @@ const router = require("express").Router();
 
 router.get('/get-by-day', CumpleañosController.getByDay)
 
+router.get('/get-by-month', CumpleañosController.getByMonth)
+
 module.exports =  router
